9b0d429fa643eb55e903b6e1ce56409a877d6867
SkinnyBuffalo <yahoomateo090@gmail.com>
2024-03-28 17:29:24 +0100

    Slozeni upiti (SVE OSIM SLIKA), i slozen prikaz aukcija i kategorija na pocetnoj stranici

--- aukcije-server/index.js
+++ aukcije-server/index.js
@@ -27,7 +27,6 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-
 app.get("/api/all-korisnik", (req, res) => {
   connection.query("SELECT * FROM korisnik", (error, results) => {
     if (error) throw error;
@@ -36,51 +35,39 @@ app.get("/api/all-korisnik", (req, res) => {
   });
 });
 
-app.post('/unosPredmeta', function (request, response) {
-    const data = request.body;
-    predmet = [[data.sifra_predmeta, data.naziv_predmeta,  data.opis_predmeta, data.slika, data.vrijeme_pocetka, data.vrijeme_zavrsetka, data.pocetna_cijena, data.svrha_donacije, data.id_korisnika, data.sifra_kategorije]]
-    connection.query('INSERT INTO predmet (sifra_predmeta, naziv_predmeta,  opis_predmeta, slika, vrijeme_pocetka, vrijeme_zavrsetka, pocetna_cijena, svrha_donacije, id_korisnika, sifra_kategorije) VALUES ?',
-    [predmet], function (error, results, fields) {
-      if (error) throw error;
-      console.log('data', data)
-      return response.send({ error: false, data: results, message: 'Predmet je dodan.' });
-    });
+app.post("/unosPredmeta", function (request, response) {
+  const data = request.body;
+  predmet = [[data.id_predmeta, data.naziv_predmeta, data.opis_predmeta, data.vrijeme_pocetka, data.vrijeme_zavrsetka, data.pocetna_cijena, data.id_korisnika, data.id_kategorije]];
+  connection.query("INSERT INTO predmet (id_predmeta, naziv_predmeta,  opis_predmeta, vrijeme_pocetka, vrijeme_zavrsetka, pocetna_cijena, id_korisnika, id_kategorije) VALUES ?", [predmet], function (error, results, fields) {
+    if (error) throw error;
+    console.log("data", data);
+    return response.send({ error: false, data: results, message: "Predmet je dodan." });
+  });
 });
 
 app.get("/api/all-predmet", (req, res) => {
-  connection.query(
-    "SELECT sifra_predmeta, naziv_predmeta, slika, pocetna_cijena, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme FROM predmet WHERE vrijeme_zavrsetka > NOW() ORDER BY preostalo_vrijeme DESC",
-    (error, results) => {
-      if (error) throw error;
+  connection.query("SELECT p.id_predmeta, p.opis_predmeta, p.naziv_predmeta, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, CONCAT( FLOOR(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka) / (24*3600)), ' dana, ', TIME_FORMAT(SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka) % (24*3600)), '%H:%i:%s') ) AS preostalo_vrijeme, s.slika FROM predmet p LEFT JOIN slika s ON p.id_predmeta = s.id_predmeta WHERE p.vrijeme_zavrsetka > NOW() ORDER BY preostalo_vrijeme DESC;", (error, results) => {
+    if (error) throw error;
 
-      res.send(results);
-    }
-  );
+    res.send(results);
+  });
 });
 
 app.get("/api/get-predmet/:id", (req, res) => {
   const { id } = req.params;
 
-  connection.query(
-    "SELECT p.naziv_predmeta, p.sifra_predmeta, p.slika, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, p.vrijeme_pocetka, p.vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, p.svrha_donacije, p.opis_predmeta, COALESCE(MAX(po.vrijednost_ponude), p.pocetna_cijena) AS vrijednost_ponude FROM predmet p LEFT JOIN ponuda po ON p.sifra_predmeta = po.sifra_predmeta WHERE p.sifra_predmeta = ?",
-    [id],
-    (error, results) => {
-      if (error) throw error;
-      res.send(results);
-    }
-  );
+  connection.query("SELECT p.naziv_predmeta, p.id_predmeta, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, p.vrijeme_pocetka, p.vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, p.opis_predmeta, COALESCE(MAX(po.vrijednost_ponude), p.pocetna_cijena) AS vrijednost_ponude FROM predmet p LEFT JOIN ponuda po ON p.id_predmeta = po.id_predmeta WHERE p.id_predmeta = ?", [id], (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
 });
 app.get("/api/get-kategorija-predmet/:id", (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    connection.query(
-        "SELECT sifra_predmeta, naziv_predmeta, slika, pocetna_cijena, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, svrha_donacije, opis_predmeta FROM predmet WHERE sifra_kategorije = ?",
-        [id],
-        (error, results) => {
-            if (error) throw error;
-            res.send(results);
-        }
-    );
+  connection.query("SELECT id_predmeta, naziv_predmeta, pocetna_cijena, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, opis_predmeta FROM predmet WHERE id_kategorije = ?", [id], (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
 });
 app.get("/api/all-kategorija", (req, res) => {
   connection.query("SELECT * FROM kategorija", (error, results) => {
@@ -88,100 +75,84 @@ app.get("/api/all-kategorija", (req, res) => {
     res.send(results);
   });
 });
-app.get('/api/all-korisnik', (req, res) => {
+app.get("/api/all-korisnik", (req, res) => {
+  connection.query("SELECT * FROM korisnik", (error, results) => {
+    if (error) throw error;
 
-    connection.query('SELECT * FROM korisnik', (error, results) => {
-        if (error) throw error;
-
-        res.send(results)
-    })
-})
-
-app.get('/api/get-predmet/:id', (req, res) => {
-    const { id } = req.params;
-
-    connection.query('SELECT sifra_predmeta, naziv_predmeta, slika, pocetna_cijena, vrijeme_pocetka, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), vrijeme_zavrsetka)), \'%H:%i:%s\' ) AS preostalo_vrijeme FROM predmet WHERE sifra_predmeta = ?', [id], (error, results) => {
-        if (error) throw error;
-        res.send(results)
-    });
+    res.send(results);
+  });
 });
 
-  app.get('/api/get-ponuda/:id', (req, res) => {
-    const { id } = req.params;
+app.get("/api/get-predmet/:id", (req, res) => {
+  const { id } = req.params;
 
-    connection.query('SELECT id_ponude, vrijednost_ponude, DATE_FORMAT(vrijeme_ponude, "%Y-%m-%d %H:%i:%s") as vrijeme_ponude, id_korisnika FROM ponuda WHERE sifra_predmeta = ?', [id], (error, results) => {
-        if (error) throw error;
-        res.send(results);
-    });
+  connection.query("SELECT id_predmeta, naziv_predmeta,pocetna_cijena, vrijeme_pocetka, vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme FROM predmet WHERE id_predmeta = ?", [id], (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
 });
 
+app.get("/api/get-ponuda/:id", (req, res) => {
+  const { id } = req.params;
 
+  connection.query('SELECT id_ponude, vrijednost_ponude, DATE_FORMAT(vrijeme_ponude, "%Y-%m-%d %H:%i:%s") as vrijeme_ponude, id_korisnika FROM ponuda WHERE id_predmeta = ?', [id], (error, results) => {
+    if (error) throw error;
+    res.send(results);
+  });
+});
 
-app.post('/unostrenutnaponuda', function (request, response) {
-  console.log('radi unos trenutna ponuda');
+app.post("/unostrenutnaponuda", function (request, response) {
+  console.log("radi unos trenutna ponuda");
   const data = request.body;
-  const ponuda = [[data.id_ponude, data.vrijednost_ponude, data.vrijeme_ponude, data.id_korisnika, data.sifra_predmeta]];
-  connection.query('INSERT INTO ponuda (id_ponude, vrijednost_ponude, vrijeme_ponude, id_korisnika, sifra_predmeta) VALUES ?',
-    [ponuda], function (error, results, fields) {
-      if (error) throw error;
-      return response.send({ error: false, data: results, message: 'Dodana je trenutna ponuda.' });
-    });
+  const ponuda = [[data.id_ponude, data.vrijednost_ponude, data.vrijeme_ponude, data.id_korisnika, data.id_predmeta]];
+  connection.query("INSERT INTO ponuda (id_ponude, vrijednost_ponude, vrijeme_ponude, id_korisnika, id_predmeta) VALUES ?", [ponuda], function (error, results, fields) {
+    if (error) throw error;
+    return response.send({ error: false, data: results, message: "Dodana je trenutna ponuda." });
+  });
 });
 app.post("/api/unos-slike", function (req, res) {
   const data = req.body;
   const slika = data.slika;
 
-  connection.query(
-    "INSERT INTO predmet (slika) VALUES (?)",
-    [slika],
-    function (error, results, fields) {
-      if (error) {
-        console.error(error);
-        return res.status(500).send({
-          error: true,
-          message: "Dogodila se greška prilikom dodavanja teksta.",
-        });
-      }
-      return res.send({
-        error: false,
-        data: results,
-        message: "Slika je dodana.",
+  connection.query("INSERT INTO slika (slika) VALUES (?)", [slika], function (error, results, fields) {
+    if (error) {
+      console.error(error);
+      return res.status(500).send({
+        error: true,
+        message: "Dogodila se greška prilikom dodavanja teksta.",
       });
     }
-  );
+    return res.send({
+      error: false,
+      data: results,
+      message: "Slika je dodana.",
+    });
+  });
 });
-app.get('/api/all-korisnik', (req, res) => {
+app.get("/api/all-korisnik", (req, res) => {
+  connection.query("SELECT * FROM korisnik", (error, results) => {
+    if (error) throw error;
 
-    connection.query('SELECT * FROM korisnik', (error, results) => {
-        if (error) throw error;
-
-        res.send(results)
-    })
-})
+    res.send(results);
+  });
+});
 
 app.get("/api/all-predmet-with-current-price", (req, res) => {
-  connection.query(
-    "SELECT p.sifra_predmeta, p.naziv_predmeta, p.slika, p.pocetna_cijena, p.vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, COALESCE(MAX(po.vrijednost_ponude), p.pocetna_cijena) AS trenutna_cijena FROM predmet p LEFT JOIN ponuda po ON p.sifra_predmeta = po.sifra_predmeta WHERE p.vrijeme_zavrsetka > NOW() GROUP BY p.sifra_predmeta ORDER BY preostalo_vrijeme DESC",
-    (error, results) => {
-      if (error) throw error;
+  connection.query("SELECT p.id_predmeta, p.naziv_predmeta,  p.opis_predmeta, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, TIME_FORMAT( SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka)), '%H:%i:%s' ) AS preostalo_vrijeme, COALESCE(MAX(po.vrijednost_ponude), p.pocetna_cijena) AS trenutna_cijena FROM predmet p LEFT JOIN ponuda po ON p.id_predmeta = po.id_predmeta WHERE p.vrijeme_zavrsetka > NOW() GROUP BY p.id_predmeta ORDER BY preostalo_vrijeme DESC", (error, results) => {
+    if (error) throw error;
 
-      res.send(results);
-    }
-  );
+    res.send(results);
+  });
 });
-app.get('/api/get-predmet-trenutna-cijena/:id', (req, res) => {
+app.get("/api/get-predmet-trenutna-cijena/:id", (req, res) => {
   const { id } = req.params;
-  connection.query(
-    "SELECT p.sifra_predmeta, p.naziv_predmeta, p.opis_predmeta, p.svrha_donacije, p.slika, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, TIME_FORMAT(SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka)), '%H:%i:%s') AS preostalo_vrijeme, COALESCE(po.trenutna_cijena, p.pocetna_cijena) AS trenutna_cijena FROM predmet p LEFT JOIN (SELECT sifra_predmeta, MAX(vrijednost_ponude) AS trenutna_cijena FROM ponuda GROUP BY sifra_predmeta) po ON p.sifra_predmeta = po.sifra_predmeta WHERE p.sifra_predmeta = ?",
-    [id],
-    (error, results) => {
-      if (error) throw error;
+  connection.query("SELECT p.id_predmeta, p.naziv_predmeta, p.opis_predmeta, p.pocetna_cijena, p.vrijeme_pocetka, p.vrijeme_zavrsetka, TIME_FORMAT(SEC_TO_TIME(TIMESTAMPDIFF(SECOND, NOW(), p.vrijeme_zavrsetka)), '%H:%i:%s') AS preostalo_vrijeme, COALESCE(po.trenutna_cijena, p.pocetna_cijena) AS trenutna_cijena FROM predmet p LEFT JOIN (SELECT id_predmeta, MAX(vrijednost_ponude) AS trenutna_cijena FROM ponuda GROUP BY id_predmeta) po ON p.id_predmeta = po.id_predmeta WHERE p.id_predmeta = ?", [id], (error, results) => {
+    if (error) throw error;
 
-      res.send(results);
-    }
-  );
+    res.send(results);
+  });
 });
 
 app.listen(port, () => {
-  console.log('Server running at port: ' + port);
+  console.log("Server running at port: " + port);
 });
--- aukcije/src/pages/Pocetna.vue
+++ aukcije/src/pages/Pocetna.vue
@@ -1,56 +1,33 @@
 <template>
   <q-card>
-    <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px"
-      >Kategorije
-    </q-item>
+    <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px">Kategorije </q-item>
     <div class="q-pa-sm row flex flex-center">
-      <div
-        v-for="item in kategorija"
-        :key="item.sifra_kategorije"
-        class="q-pa-md"
-        style="width: 400px"
-      >
-        <q-card @click="navigateToItem1(item.sifra_kategorije)">
+      <div v-for="item in kategorija" :key="item.id_kategorije" class="q-pa-md" style="width: 400px">
+        <q-card @click="navigateToItem1(item.id_kategorije)">
           <q-item-section>
-            <q-item class="q-pa-sm text-bold text-blue-7"
-              >{{ item.naziv_kategorije }}
-            </q-item>
+            <q-item class="q-pa-sm text-bold text-blue-7">{{ item.naziv_kategorije }} </q-item>
           </q-item-section>
         </q-card>
       </div>
     </div>
 
-
-  <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px"
-    >Zadnje ili trenutne aukcije
-  </q-item>
-  <div class="q-pa-sm row flex flex-center">
-    <div
-      v-for="item in items"
-      :key="item.sifra_predmeta"
-      class="q-pa-md"
-      style="width: 400px"
-    >
-      <q-card @click="navigateToItem(item.sifra_predmeta)">
-        <q-img :src="item.slika" no-native-menu />
-        <q-item-section>
-          <q-item class="q-pa-sm text-bold text-blue-7"
-            >{{ item.naziv_predmeta }}
-          </q-item>
-          <q-item>Početna cijena: {{ item.pocetna_cijena }}$</q-item>
-          <q-item
-            >Vrijeme zavrsetka:
-            {{ formattedDate(item.vrijeme_zavrsetka) }}</q-item
-          >
-          <q-item
-            >Preostalo vrijeme aukcije: {{ item.preostalo_vrijeme }} h
-          </q-item>
-          <q-item>Trenutna cijena: {{ item.trenutna_cijena }}$</q-item>
-        </q-item-section>
-      </q-card>
+    <q-item class="q-pa-sm text-bold text-blue-7" style="font-size: 30px">Zadnje ili trenutne aukcije </q-item>
+    <div class="q-pa-sm row flex flex-center">
+      <div v-for="item in items" :key="item.id_predmeta" class="q-pa-md" style="width: 400px">
+        <q-card @click="navigateToItem(item.id_predmeta)">
+          <q-img :src="item.slika" no-native-menu />
+          <q-item-section>
+            <q-item class="q-pa-sm text-bold text-blue-7">{{ item.naziv_predmeta }} </q-item>
+            <q-item>Početna cijena: {{ item.pocetna_cijena }}$</q-item>
+            <q-item>Vrijeme pocetka: {{ formattedDate(item.vrijeme_pocetka) }}</q-item>
+            <q-item>Vrijeme zavrsetka: {{ formattedDate(item.vrijeme_zavrsetka) }}</q-item>
+            <q-item>Preostalo vrijeme aukcije: {{ item.preostalo_vrijeme }} h </q-item>
+            <q-item>Trenutna cijena: {{ item.trenutna_cijena }}$</q-item>
+          </q-item-section>
+        </q-card>
+      </div>
     </div>
-  </div>
-</q-card>
+  </q-card>
 </template>
 
 <script>
@@ -69,7 +46,7 @@ export default {
     };
   },
   mounted() {
-    axios.get(baseUrl + "all-predmet-with-current-price").then((response) => {
+    axios.get(baseUrl + "all-predmet").then((response) => {
       this.items = response.data;
     });
     axios.get(baseUrl + "all-kategorija").then((response) => {
@@ -82,11 +59,11 @@ export default {
       return new Date(dateString).toLocaleString("hr-HR").replace(",", "");
     },
 
-    navigateToItem(sifra_predmeta) {
-      this.$router.push({ path: "prikaz", query: { sifra_predmeta } });
+    navigateToItem(id_predmeta) {
+      this.$router.push({ path: "prikaz", query: { id_predmeta } });
     },
-    navigateToItem1(sifra_kategorije) {
-      this.$router.push({ path: "kategorija", query: { sifra_kategorije } });
+    navigateToItem1(id_kategorije) {
+      this.$router.push({ path: "kategorija", query: { id_kategorije } });
     },
   },
 };
